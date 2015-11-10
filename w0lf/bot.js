b4943872c2624b69ca497a76398e266317e725d2
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2015-11-09 21:21:20 -0800

    BFS path finding outline

--- w0lf/bot.js
+++ w0lf/bot.js
@@ -3,18 +3,27 @@ var _ = require('lodash'),
 
 var dirs = 'nesw';
 
-function bot(state, callback) {
-	var i = Math.floor(Math.random() * 4);
-	var dir = dirs[i];
-	callback(null, dir);
-};
-
 function getTile(map, x, y) {
 	var size = map.size;
 	var tiles = map.tiles;
 	assert.strictEqual(x < size && y < size, true, 'coordinates out of bounds');
 	return tiles[y * size + x];
 }
+
+function pathFind(map, p1, p0) {
+	var p = p0;
+	while (!p1.eql(p)) {
+		// BFS for p1 from p0
+	}
+}
+
+function bot(state, callback) {
+	console.log(state);
+	parseMap(state.board);
+	var i = Math.floor(Math.random() * 4);
+	var dir = dirs[i];
+	callback(null, dir);
+};
 
 module.exports = bot;
 if (require.main === module)
