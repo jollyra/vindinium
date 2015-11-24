92004d94224a464f197e4fa348946bc03b68b458
Nigel Rahkola <jollyra@outlook.com>
2015-11-23 22:42:03 -0800

    Add test for pathfinding algorithm

---
+++ w0lf/test/pathFinderTest.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+var Point = require('../point');
+
+describe('#pathFind', function() {
+	it('should find path from p0 to p1', function () {
+		assert.equal(true, true);
+	});
+});
