8b0a27deb5efda085ee0f8df00c3c0e749883905
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2015-11-28 21:33:11 -0800

    Get tile from stick when tile is 2 chars

--- w0lf/bot.js
+++ w0lf/bot.js
@@ -4,8 +4,15 @@ var _ = require('lodash'),
 
 var dirs = 'nesw';
 
+/*
+ * Each tile is represented by two characters, and the map is a
+ * one-dimensional array or a 'stick' of tiles.
+ */
 function getTile(map, x, y) {
+	var tileSize = 2;
+	var tileIdx = (y * map.size * tileSize) + (x * tileSize);
 	assert.equal(tileIdx < map.tiles.length, true, 'coordinates out of bounds');
+	return map.tiles.substr(tileIdx, tileSize);
 }
 
 function pathFind(map, p1, p0) {
